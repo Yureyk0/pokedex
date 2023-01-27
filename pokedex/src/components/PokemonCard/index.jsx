d068316238c29b97ba37e4bff58648527d12ea99
Yura <yuraboreyko@gmail.com>
2023-01-27 19:47:39 +0200

    layot

--- pokedex/src/components/PokemonCard/index.jsx
+++ pokedex/src/components/PokemonCard/index.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import { useState } from 'react'
 
 function PokemonCard({ item }) {
+  const [isShown, setIsShown] = useState(false)
+  const [currentPokemon, setCurrentPokemon] = useState()
   return (
     <>
-      <li>{item.name}</li>
+      <div className="pokemon-list">
+        <div
+          className="pokemon-item-li"
+          onMouseEnter={() => setIsShown(true)}
+          onMouseLeave={() => setIsShown(false)}
+        >
+          <div className="pokemon-item-li-left-content">
+            <img
+              src={item.sprites.front_default}
+              alt={item.sprites.front_default}
+            />
+            <p>{`№ ${item.id}`}</p>
+          </div>
+          <p>{item.name.toUpperCase()}</p>
+        </div>
+        {isShown && <div className="right-pokemon-card">{item.name}</div>}
+      </div>
     </>
   )
 }
--- pokedex/src/components/PokemonsGrid/index.jsx
+++ pokedex/src/components/PokemonsGrid/index.jsx
@@ -1,14 +1,40 @@
 import React from 'react'
+import PokemonsListItem from '../PokemonListItem'
 import PokemonCard from '../PokemonCard'
+import { useState } from 'react'
+import './PokemonsGrid.css'
 
 function PokemonsGrid({ pokemonsList }) {
+  // const [isShown, setIsShown] = useState(false)
+  // const [currentPokemon, setCurrentPokemon] = useState()
   return (
     <>
-      <ol>
-        {pokemonsList.map((res) => {
-          return <PokemonCard key={res.id} item={res} />
+      {/* <div className="pokemon-list">
+        {pokemonsList.map((item) => {
+          return (
+            <>
+              <div
+                className="pokemon-item-li"
+                onMouseEnter={() => setIsShown(true)}
+                onMouseLeave={() => setIsShown(false)}
+              >
+                <div className="pokemon-item-li-left-content">
+                  <img
+                    src={item.sprites.front_default}
+                    alt={item.sprites.front_default}
+                  />
+                  <p>{`№ ${item.id}`}</p>
+                </div>
+                <p>{item.name.toUpperCase()}</p>
+              </div>
+              {isShown && <div className="right-pokemon-card">{item.name}</div>}
+            </>
+          )
         })}
-      </ol>
+      </div> */}
+      {pokemonsList.map((item) => {
+        return <PokemonCard item={item} />
+      })}
     </>
   )
 }
